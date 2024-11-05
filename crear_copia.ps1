7d42736dc500682a5d3345cfc68a7ae914cced30
olanav <olanavillalon01.cf@iesesteveterradas.cat>
2024-11-05 16:55:52 +0100

    Add files via upload

---
+++ crear_copia.ps1
@@ -0,0 +1,14 @@
+﻿function copiar{
+    param (
+        $rutaorigen,
+        $rutadesti
+    )
+    if ((Test-Path $rutaorigen) -And (Test-Path $rutadesti)) {
+        Copy-Item $rutaorigen $rutadesti
+        Write-Host "La copia s'ha realitzat correctament."
+    }
+    else {
+        Write-Host "Alguna de les rutes introduides no existeixen."
+    }
+  
+}
---
+++ localitzar.ps1
@@ -0,0 +1,14 @@
+﻿function localitzar{
+    param (
+        $ruta,
+        $fitx
+    )
+
+    $res = Get-Childitem -Path $ruta | Where-Object Name -eq $fitx
+    if ($res -eq $null) {
+        Write-Host "El fitxer que estàs buscant no es trova a la ruta indicada."
+    }
+    else {
+        Write-Host "El fitxer" $fitx "existeix a la ruta" $ruta"."
+    }
+}
