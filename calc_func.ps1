874c7f2dd35a11cedd84fbe8ccbe581ab4f3a275
olanav <olanavillalon01.cf@iesesteveterradas.cat>
2024-11-12 16:24:02 +0100

    Add files via upload

---
+++ calc_func.ps1
@@ -0,0 +1,30 @@
+﻿function calcu{
+    param (
+        [int]$num1,
+        [int]$num2,
+        [string]$op
+    )
+        if ($op -eq "+") {
+            $res = $num1 + $num2
+        }
+        elseif ($op -eq "-") {
+            $res = $num1 - $num2
+        }
+        elseif ($op -eq "*" ){
+            $res = $num1 * $num2
+        }
+        elseif ($op -eq "/" ){
+            if ($num2 -eq 0) {
+                Write-Host "No es pot dividir entre zero"
+                continue
+            }
+            else {
+                $res = $num1 / $num2
+            }
+        }
+        else {
+        Write-Host "No has introduit una opció vàlida"
+        continue
+        }
+        Write-Host "El resultat és:"$res
+    }
---
+++ locate_pc.ps1
@@ -0,0 +1,28 @@
+﻿function locate_pc{
+    param (
+        $fitx
+    )
+    while ($true){
+        $res = Get-Childitem $fitx -Recurse -ErrorAction SilentlyContinue
+        $path=$res.DirectoryName
+        if ($res -eq $null) {
+            Write-Host "El fitxer que estàs buscant no es trova a l'equip."
+        }
+        else {
+            Write-Host "El fitxer $fitx es troba a $path."
+        }
+        $cont = Read-Host "Vols continuar? (S/N)"
+        if ($cont -eq "N") {
+           Read-Host "Sortint..."
+           break
+        }
+        elseif ($cont -eq "S") {
+            $fitx = Read-Host "Introdueix el nou fitxer a buscar"
+            continue
+        }
+        else {
+            Read-Host "Has introduit una opció invàlida"
+            continue
+        }
+    }
+}
