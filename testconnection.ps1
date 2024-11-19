b1a0b4f8fc6b6134d84e91fd2894683614fe6b6b
olanav <olanavillalon01.cf@iesesteveterradas.cat>
2024-11-19 17:16:50 +0100

    Add files via upload

---
+++ testconnection.ps1
@@ -0,0 +1,7 @@
+﻿$res = Test-NetConnection -ComputerName 8.8.8.8 -port 53 -InformationLevel Quiet
+if ($res -eq $True){
+     [System.Windows.MessageBox]::Show("Connexió TCP correcte", "Connexió TCP", "OK")
+}
+else {
+     [System.Windows.MessageBox]::Show("Connexió TCP incorrecte", "Connexió TCP", "OK")
+}
