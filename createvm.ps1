7af4f360565d8d2dd1695a4a2953c3b8bf41208a
olanav <olanavillalon01.cf@iesesteveterradas.cat>
2024-12-03 16:59:41 +0100

    Add files via upload

---
+++ createvm.ps1
@@ -0,0 +1,18 @@
+﻿$nom = Read-Host "Digues el nom de la teva MV"
+$os = Read-Host "Digues quin SO farás servir (Windows10/Linux26)"
+$num = 1
+if ($os -eq "Windows10" -OR $os -eq "Linux26") {
+    VBoxManage createvm --name $nom --ostype $os --register
+    VBoxManage modifyvm $nom --nic1 nat
+    VBoxManage modifyvm $nom --nictype1 virtio
+    VBoxManage modifyvm $nom --natnet1 default
+    while ($num -ne 8){
+        $num = $num + 1
+        VBoxManage modifyvm $nom --nic$num intnet
+        VBoxManage modifyvm $nom --nictype$num virtio
+        VBoxManage modifyvm $nom --intnet$num intnet 
+    } 
+}
+else {
+    Write-Host "No has introduit un SO vàlid."
+}
---
+++ vm_start.ps1
@@ -0,0 +1,31 @@
+﻿$opc = Write-Host "Que vols fer (Iniciar / Guardar / Aturar)?"
+VBoxManage list vms
+$maq = Write-Host "Amb quina màquina vols treballar?"
+if ($opc -eq "Iniciar"){
+    Write-Host "Iniciant la màquina..."
+    try {
+        VBoxManage startvm $maq
+    } catch {
+    Write-Host "Ha sorgit un error, comprova que la teva màquina apareix aquí:"
+    VBoxManage list vms
+    }
+}
+elsif ($opc -eq "Aturar"){
+    try {
+    VBoxManage controlvm $maq poweroff
+    } catch {
+    Write-Host "Ha sorgit un error, comprova que la teva màquina apareix aquí:"
+    VBoxManage list runnningvms
+}
+}
+elsif ($opc -eq "Guardar"){
+       try {
+    VBoxManage controlvm $maq savestate
+    } catch {
+    Write-Host "Ha sorgit un error, comprova que la teva màquina apareix aquí:"
+    VBoxManage list runnningvms 
+}
+else {
+    Write-Host "Has introduït una opció no vàlida"   
+}
+}
